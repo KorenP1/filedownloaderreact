17c163939078c2f8ee78e51f467f798bf348c3c4
Kope <Kope@Kope.com>
2024-02-25 01:01:28 +0200

    W

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -2,15 +2,27 @@ import './App.css'
 import { Button, LinearProgress } from '@mui/material';
 import { DownloadRounded, PlayCircleOutlineRounded, DeleteOutlineRounded } from '@mui/icons-material';
 
+function start() {
+    alert('START')
+}
+
+function rmrf() {
+    alert('DELETE')
+}
+
+function download() {
+    alert('DOWNLOAD')
+}
+
 const App = () => {
     return (
         <>
             <h1>Noder</h1>
 
             <div className='buttons'>
-                <Button color='error' variant='contained' size='large' startIcon={<DeleteOutlineRounded />}>rm -rf /</Button>
-                <Button color='success' variant='contained' size='large' startIcon={<PlayCircleOutlineRounded />}>Start</Button>
-                <Button color='secondary' variant='contained' size='large' startIcon={<DownloadRounded />}>Download</Button>
+                <Button color='error' variant='contained' size='large' startIcon={<DeleteOutlineRounded />} onClick={rmrf}>rm -rf /</Button>
+                <Button color='success' variant='contained' size='large' startIcon={<PlayCircleOutlineRounded />} onClick={start}>Start</Button>
+                <Button color='secondary' variant='contained' size='large' startIcon={<DownloadRounded />} onClick={download}>Download</Button>
             </div>
 
             <LinearProgress id='progress' color='secondary'></LinearProgress>
